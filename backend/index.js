fccbe23cc0709dd863fcaf41608efa733e892f8e
einartech <imalovelyghost13@gmail.com>
2024-01-03 13:16:58 +0100

    logs added

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,51 @@
-// index.js: El punto de entrada principal que utiliza los módulos anteriores.
 import express from "express";
 import cors from "cors";
 import connectDB from "./db/connectdb.js";
-import itemRoutes from "./routes/ItemRoute.js"; // Cambiado el nombre del import
-import companyRoutes from "./routes/CompanyRoute.js"; // Cambiado el nombre del import
+import itemRoutes from "./routes/ItemRoute.js";
+import companyRoutes from "./routes/CompanyRoute.js";
+import winston from "winston";
+import { fileURLToPath } from 'url'; // Importa el módulo 'url'
+import path from "path";
+import fs from "fs"; // Importa el módulo 'fs'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Configuración del directorio de logs
+const logsDirectory = path.join(__dirname, 'logs');
+
+// Asegúrate de que el directorio logs exista
+if (!fs.existsSync(logsDirectory)) {
+  fs.mkdirSync(logsDirectory);
+}
+
+// Configuración básica de winston
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: path.join(logsDirectory, 'logfile.log') })
+  ],
+});
+
 app.use(express.json());
 app.use(cors({
-  origin: '*',  // Reemplaza con la URL de tu aplicación cliente
-  credentials: true,  // Habilita el intercambio de cookies y encabezados de autenticación
+  origin: '*',
+  credentials: true,
 }));
+
+// Middleware para registrar solicitudes
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
 
 // Conectar a la base de datos
 connectDB();
@@ -25,5 +55,5 @@ app.use(itemRoutes);
 app.use(companyRoutes);
 
 app.listen(port, () => {
-  console.log(`Servidor API en http://localhost:${port}`);
+  logger.info(`Servidor API en http://localhost:${port}`);
 });
